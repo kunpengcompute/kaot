0c71874bd08b08a415994318a64bc94211c6f7b7
leiqing10 <leiqing10@h-partners.com>
2025-12-23 15:54:05 +0800

    add license

--- src/feature_manager/feature/config_core_isolation.py
+++ src/feature_manager/feature/config_core_isolation.py
@@ -1,9 +1,25 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# Copyright 2025 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===========================================================================
 import os
 import subprocess
 import re
-from kaot.feature_manager.feature import register_feature
-from kaot.feature_manager.feature.base import BaseFeature
-from kaot.utils.log import get_logger
+from src.feature_manager.feature import register_feature
+from src.feature_manager.feature.base import BaseFeature
+from src.utils.log import get_logger
 
 logger = get_logger(__name__)
 
