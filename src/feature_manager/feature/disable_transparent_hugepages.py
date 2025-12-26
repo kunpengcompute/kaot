2b2453cf0e614cf829848dc9ca4baa5108180f6b
HeZimian <hezimian@huawei.com>
2025-12-26 11:08:28 +0800

    日志优化

--- src/feature_manager/feature/disable_transparent_hugepages.py
+++ src/feature_manager/feature/disable_transparent_hugepages.py
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ===========================================================================
-import subprocess
 import re
 from src.feature_manager.feature import register_feature
 from src.feature_manager.feature.base import BaseFeature
